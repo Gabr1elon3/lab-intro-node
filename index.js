d938ce1e3853a2c2462578a7c34dbca8b310d234
Gabriele Capezzuto <gabricap7@gmail.com>
2025-03-13 19:40:53 +0100

    new

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length++;
+  }
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
+  max() {
+    if (this.items.length > 0){
+      const highest = Math.max(...this.items);
+      return highest;
+    }else{
+     throw new Error("OutOfBounds");
+    }  
+  }
 
-  get(pos) {}
+  min() {
+    if (this.items.length > 0){
+      const highest = Math.min(...this.items);
+      return highest;
+    }else{
+     throw new Error("OutOfBounds");
+    }  
 
-  max() {}
+  }
 
-  min() {}
+  sum() {
+    if (this.items.length >0){
+      
+      const sum= this.items.reduce((accumulator, currentValue)=> accumulator+ currentValue, 0)
+    return sum;
+    } else {
+      return 0;
+     }  
+    
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length >0){
+      
+      const sum= this.items.reduce((accumulator, currentValue)=> accumulator+ currentValue, 0)
+    const average= sum/this.items.length;
+    return average;
+    } else {
+      throw new Error("OutOfBounds");
+     }  
+      
+  }
 }
 
 module.exports = SortedList;
